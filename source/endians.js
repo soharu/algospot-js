98be4dc47d805868af7b72d5a74d8e9670ddadb5
Jahyun Oh <soharu1024@gmail.com>
2019-01-31 23:51:31 +0900

    Refactor solve() for ENDIANS

--- source/endians.js
+++ source/endians.js
@@ -1,51 +1,21 @@
-class Bin32 {
-    constructor(n) {
-        this.n = n
-        let bytes = [];
-        for (let i = 0; i < 4; ++i) {
-            let s = '';
-            for (let j = 0; j < 8; ++j) {
-                let bit = n % 2;
-                n = Math.floor(n / 2);
-                s = bit + s;
-            }
-            bytes.push(s);
-        }
-        this.bytes = bytes.reverse();   // store bytes in big endian order
-    }
-
-    littleEndian(delimiter) {
-        return new Array(...this.bytes).reverse().join(delimiter);
-    }
-
-    bigEndian(delimiter) {
-        return this.bytes.join(delimiter);
-    }
-
-    get bytesReversed() {
-        return parseInt(this.littleEndian(''), 2);
-    }
-}
-
 function solve(n) {
-    return new Bin32(n).bytesReversed;
+    return (((n & 0xFF) << 24)
+        | ((n & 0xFF00) << 8)
+        | ((n & 0xFF0000) >> 8)
+        | ((n & 0xFF000000) >> 24)) >>> 0;
 }
 
 if (require.main === module) {
-    const readline = require('readline');
-    const rl = readline.createInterface({
+    const rl = require('readline').createInterface({
         input: process.stdin
     });
-    let hasCases = false;
+    let lines = [];
     rl.on('line', (line) => {
-        if (hasCases == false) {
-            hasCases = true;
-        } else {
-            const n = parseInt(line, 10);
-            console.log(solve(n));
-        }
+        lines.push(line.trim());
+    });
+    rl.on('close', () => {
+        lines.slice(1, lines.length).forEach((n) => console.log(solve(n)));
     });
 } else {
-    exports.Bin32 = Bin32;
     exports.solve = solve;
 }
--- test/endians-test.js
+++ test/endians-test.js
@@ -1,88 +1,13 @@
 const assert = require('assert');
 const endians = require('../source/endians.js');
 
-const Bin32 = endians.Bin32;
-
 describe('ENDIANS', function() {
-    describe('Bin32', function() {
-        it('represents 32-bits binary number: n = 0', function() {
-            let b = new Bin32(0);
-            assert.strictEqual(b.n, 0);
-            assert.strictEqual(b.bytes[3], '00000000');
-            assert.strictEqual(b.bytes[2], '00000000');
-            assert.strictEqual(b.bytes[1], '00000000');
-            assert.strictEqual(b.bytes[0], '00000000');
-            assert.strictEqual(b.bigEndian('_'), '00000000_00000000_00000000_00000000');
-            assert.strictEqual(b.littleEndian('_'), '00000000_00000000_00000000_00000000');
-        });
-
-        it('represents 32-bits binary number: n = 1', function() {
-            let b = new Bin32(1);
-            assert.strictEqual(b.n, 1);
-            assert.strictEqual(b.bytes[3], '00000001');
-            assert.strictEqual(b.bytes[2], '00000000');
-            assert.strictEqual(b.bytes[1], '00000000');
-            assert.strictEqual(b.bytes[0], '00000000');
-            assert.strictEqual(b.littleEndian('_'), '00000001_00000000_00000000_00000000');
-            assert.strictEqual(b.bigEndian('_'), '00000000_00000000_00000000_00000001');
-            assert.strictEqual(b.bytesReversed, 16777216);
-        });
-
-        it('represents 32-bits binary number: n = 305419896', function() {
-            let b = new Bin32(305419896);
-            assert.strictEqual(b.n, 305419896);
-            assert.strictEqual(b.bytes[3], '01111000');
-            assert.strictEqual(b.bytes[2], '01010110');
-            assert.strictEqual(b.bytes[1], '00110100');
-            assert.strictEqual(b.bytes[0], '00010010');
-            assert.strictEqual(b.littleEndian('_'), '01111000_01010110_00110100_00010010');
-            assert.strictEqual(b.bigEndian('_'), '00010010_00110100_01010110_01111000');
-            assert.strictEqual(b.bytesReversed, 2018915346);
-        });
-
-        it('represents 32-bits binary number: n = 0x12345678', function() {
-            let b = new Bin32(0x12345678);
-            assert.strictEqual(b.n, 0x12345678);
-            assert.strictEqual(b.bytes[3], '01111000');
-            assert.strictEqual(b.bytes[2], '01010110');
-            assert.strictEqual(b.bytes[1], '00110100');
-            assert.strictEqual(b.bytes[0], '00010010');
-            assert.strictEqual(b.littleEndian('_'), '01111000_01010110_00110100_00010010');
-            assert.strictEqual(b.bigEndian('_'), '00010010_00110100_01010110_01111000');
-            assert.strictEqual(b.bytesReversed, 2018915346);
-        });
-
-        it('represents 32-bits binary number: n = 0xFFFFFFFF', function() {
-            let b = new Bin32(0xFFFFFFFF);
-            assert.strictEqual(b.n, 0xFFFFFFFF);
-            assert.strictEqual(b.bytes[3], '11111111');
-            assert.strictEqual(b.bytes[2], '11111111');
-            assert.strictEqual(b.bytes[1], '11111111');
-            assert.strictEqual(b.bytes[0], '11111111');
-            assert.strictEqual(b.littleEndian('_'), '11111111_11111111_11111111_11111111');
-            assert.strictEqual(b.bigEndian('_'), '11111111_11111111_11111111_11111111');
-            assert.strictEqual(b.bytesReversed, 4294967295);
-        });
-    });
-
     describe('solve', function() {
         it('should return a number reversed bytes order', function() {
             assert.strictEqual(endians.solve(2018915346), 305419896);
             assert.strictEqual(endians.solve(1), 16777216);
             assert.strictEqual(endians.solve(100000), 2693136640);
             assert.strictEqual(endians.solve(4294967295), 4294967295);
-        });
-    });
-
-    describe('parseInt', function() {
-        it('convert integer string to hex number', function() {
-            assert.strictEqual(parseInt('10', 2), 2);
-            assert.strictEqual(parseInt('1', 2), 1);
-            assert.strictEqual(parseInt('010', 2), 2);
-            assert.strictEqual(parseInt('1010', 2), 10);
-            assert.strictEqual(parseInt('00001000', 2), 8);
-            assert.strictEqual(parseInt('0x10', 16), 16);
-            assert.strictEqual(parseInt('00000001000000000000000000000000', 2), 16777216)
         });
     });
 });
