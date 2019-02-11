551eb5d7475d28c366fe8d83086ed02b240efe5d
Jahyun Oh <soharu1024@gmail.com>
2019-02-11 23:54:46 +0900

    Try to solve WEIRD... (current state: WRONG ANSWER)

--- source/weird.js
+++ source/weird.js
@@ -1,3 +1,36 @@
+const primitiveWeridNumbers = new Set([
+  70, 836, 4030, 5830, 7192, 7912, 9272, 10792, 17272, 45356, 73616, 83312, 91388, 113072
+]);
+
+function generatePrimeNumberTable(numberLimit) {
+  const notPrime = 1;
+  const table = new Array(numberLimit + 1);
+  for (let i = 0; i <= numberLimit; ++i) {
+    table[i] = 0;
+  }
+
+  // 0 and 1 is not a prime number
+  table[0] = notPrime;
+  table[1] = notPrime;
+
+  for (let i = 1; i <= numberLimit; ++i) {
+    if (table[i] === 1) {
+      continue;
+    }
+
+    for (let j = i * 2; j <= numberLimit; j += i) {
+      table[j] = notPrime;
+    }
+  }
+
+  return table;
+}
+
+const isPrimeNumber = (function () {
+  const primeNumberTable = generatePrimeNumberTable(3000);
+  return number => primeNumberTable[number] === 0;
+})();
+
 // number: a natural number greater than 1
 function getProperDivisors(number) {
   const divisorSet = new Set([1]);
@@ -15,52 +48,42 @@ function getProperDivisors(number) {
   return [...divisorSet].sort((a, b) => a - b);
 }
 
-function generateBitArray(number, bitLength) {
-  const bits = [];
-  let n = number;
-
-  for (let i = 0; i < bitLength; ++i) {
-    bits.push(n & 1);
-    n >>= 1;
-  }
-
-  return bits.reverse();
-}
-
-function * subsets(numbers) {
-  const bitLength = numbers.length;
-  const numberOfSubsets = (2 ** bitLength) - 1;
-  for (let i = 1; i <= numberOfSubsets; ++i) {
-    const bits = generateBitArray(i, bitLength);
-    yield numbers.filter((n, i) => bits[i] === 1);
-  }
-}
-
 function sum(numbers) {
   return numbers.reduce((acc, n) => acc + n, 0);
 }
 
 function isWeird(number) {
-  const properDivisors = getProperDivisors(number);
+  // It is not known if any odd weird numbers exist. If so, they must be greater than 10^21
+  if (number % 2 === 1) {
+    return false;
+  }
 
+  if (primitiveWeridNumbers.has(number)) {
+    return true;
+  }
+
+  const properDivisors = getProperDivisors(number);
   if (sum(properDivisors) < number) {
     return false;
   }
 
-  for (const subset of subsets(properDivisors)) {
-    if (sum(subset) === number) {
-      return false;
+  const pwns = [...primitiveWeridNumbers];
+  for (let i = 0; i < pwns.length; ++i) {
+    if (number % pwns[i] !== 0) {
+      continue;
+    }
+
+    const p = Math.trunc(number / pwns[i]);
+    if (isPrimeNumber(p) && p > sum(getProperDivisors(p))) {
+      return true;
     }
   }
 
-  return true;
-}
-
-function check(number) {
-  return isWeird(number) ? 'weird' : 'not weird';
+  return false;
 }
 
 if (require.main === module) {
+  const check = number => isWeird(number) ? 'weird' : 'not weird';
   const inputs = [];
   require('readline')
     .createInterface({
@@ -75,10 +98,8 @@ if (require.main === module) {
       }
     });
 } else {
+  exports.generatePrimeNumberTable = generatePrimeNumberTable;
   exports.getProperDivisors = getProperDivisors;
-  exports.generateBitArray = generateBitArray;
-  exports.subsets = subsets;
   exports.sum = sum;
   exports.isWeird = isWeird;
-  exports.check = check;
 }
--- test/weird-test.js
+++ test/weird-test.js
@@ -2,6 +2,20 @@ const assert = require('assert');
 const weird = require('../source/weird.js');
 
 describe('WEIRD', () => {
+  describe('generatePrimeNumberTable()', () => {
+    it('should generate prime number table that the prime numbers are less than a number limt', () => {
+      assert.deepStrictEqual(weird.generatePrimeNumberTable(10),
+        [1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1]);
+    });
+
+    it('should generate prime number table that the prime numbers are less than 3000', () => {
+      const table = weird.generatePrimeNumberTable(3000);
+      assert.deepStrictEqual(table.filter(v => v === 0).length, 430);
+      assert.strictEqual(table[2473], 0);
+      assert.strictEqual(table[2999], 0);
+    });
+  });
+
   describe('getProperDivisors()', () => {
     it('should return an array that contains proper divisors of a number', () => {
       assert.deepStrictEqual(weird.getProperDivisors(2), [1]);
@@ -13,31 +27,6 @@ describe('WEIRD', () => {
       assert.deepStrictEqual(weird.getProperDivisors(20), [1, 2, 4, 5, 10]);
       assert.deepStrictEqual(weird.getProperDivisors(70), [1, 2, 5, 7, 10, 14, 35]);
       assert.deepStrictEqual(weird.getProperDivisors(1024), [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]);
-    });
-  });
-
-  describe('generateBitArray()', () => {
-    it('should generate a bit array of the number', () => {
-      assert.deepStrictEqual(weird.generateBitArray(1, 3), [0, 0, 1]);
-      assert.deepStrictEqual(weird.generateBitArray(2, 3), [0, 1, 0]);
-      assert.deepStrictEqual(weird.generateBitArray(3, 3), [0, 1, 1]);
-      assert.deepStrictEqual(weird.generateBitArray(4, 3), [1, 0, 0]);
-      assert.deepStrictEqual(weird.generateBitArray(5, 3), [1, 0, 1]);
-      assert.deepStrictEqual(weird.generateBitArray(6, 3), [1, 1, 0]);
-      assert.deepStrictEqual(weird.generateBitArray(7, 3), [1, 1, 1]);
-    });
-  });
-
-  describe('subset generator', () => {
-    it('should generate subsets of array', () => {
-      const subsets = weird.subsets([1, 2, 3]);
-      assert.deepStrictEqual(subsets.next().value, [3]);
-      assert.deepStrictEqual(subsets.next().value, [2]);
-      assert.deepStrictEqual(subsets.next().value, [2, 3]);
-      assert.deepStrictEqual(subsets.next().value, [1]);
-      assert.deepStrictEqual(subsets.next().value, [1, 3]);
-      assert.deepStrictEqual(subsets.next().value, [1, 2]);
-      assert.deepStrictEqual(subsets.next().value, [1, 2, 3]);
     });
   });
 
@@ -55,6 +44,7 @@ describe('WEIRD', () => {
       assert.strictEqual(weird.isWeird(3), false);
       assert.strictEqual(weird.isWeird(8), false);
       assert.strictEqual(weird.isWeird(9), false);
+      assert.strictEqual(weird.isWeird(11), false);
       assert.strictEqual(weird.isWeird(12), false);
       assert.strictEqual(weird.isWeird(20), false);
       assert.strictEqual(weird.isWeird(70), true);
@@ -64,21 +54,10 @@ describe('WEIRD', () => {
       assert.strictEqual(weird.isWeird(14770), true);
       assert.strictEqual(weird.isWeird(17724), false);
       assert.strictEqual(weird.isWeird(56322), false);
-    });
-
-    it('should check whether two hundred numbers are weird in 3 seconds', () => {
-      const maxNumber = 500000;
-      for (let i = 0; i < 200; ++i) {
-        const n = Math.trunc(maxNumber * Math.random());
-        weird.isWeird(n);
-      }
-    }).timeout(3000);
-  });
-
-  describe('check()', () => {
-    it('should check whether a number is weird', () => {
-      assert.strictEqual(weird.check(12), 'not weird');
-      assert.strictEqual(weird.check(70), 'weird');
+      assert.strictEqual(weird.isWeird(10430), true);
+      assert.strictEqual(weird.isWeird(10570), true);
+      assert.strictEqual(weird.isWeird(10990), true);
+      assert.strictEqual(weird.isWeird(11410), true);
     });
   });
 });
