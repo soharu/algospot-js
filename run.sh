73323f94a207fa92574e1a046df63a66b08c8c81
Jahyun Oh <soharu1024@gmail.com>
2021-02-17 17:49:16 +0900

    Update run.sh to run all js

--- run.sh
+++ run.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env bash
-if [ $# == 0 ]; then
+
+upper() {
+    echo $(echo ${@%.js} | tr '[:lower:]' '[:upper:]')
+}
+
+lower() {
+    echo $(echo $1 | tr '[:upper:]' '[:lower:]')
+}
+
+run_js() {
+    name=$(lower $1)
+    printf "%s\n" "$(node source/$1.js < data/$1.in)"
+}
+
+if [[ $# == 0 ]]; then
     echo "Usage: $0 {problem_id}"
     exit 1
-elif [ $# == 1 ]; then
-    name="$(echo $1 | tr '[A-Z]' '[a-z]')"
-    node source/${name}.js < data/${name}.in
-elif [ $# == 2 ] && [ $2 == "--diff" ]; then
-    name="$(echo $1 | tr '[A-Z]' '[a-z]')"
-    node source/${name}.js < data/${name}.in | diff data/${name}.out -
 fi
+
+if [[ $# == 2 ]] && [[ $2 == "--diff" ]]; then
+    diff_enabled=true
+fi
+
+problem_ids="$(ls source | sed 's/\.js$//')"
+problem_id=$(lower $1)
+
+if [[ ${problem_id} == "all" ]]; then
+    sources=${problem_ids}
+else
+    sources=($problem_id)
+fi
+
+for name in ${sources}; do
+    echo -e "\n>>> Run $(upper $name)"
+    if [[ ${diff_enabled} == true ]]; then
+        echo $(run_js $name | diff data/${name}.out -)
+    else
+        run_js $name
+    fi
+done
